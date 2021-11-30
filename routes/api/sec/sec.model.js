0efd74781f2ad3bd9a5a13c4cc355b5cbc4b22ea
Nats <natamador1976@gmail.com>
2021-11-29 19:47:58 -0600

    sec-model

--- routes/api/sec/sec.model.js
+++ routes/api/sec/sec.model.js
@@ -1,1 +1,51 @@
+var conn= require('../../../utils/dao');
+var ObjectID= require('mongodb').ObjectId;
+const bcrypt=require("bcryptjs");
 
+var _db; 
+class Sec{
+    secColl =null; 
+    constructor(){
+        this.initModel();
+    }
+    async initModel(){
+        try {
+            _db=await conn.getDB();
+            this.secColl= await _db.collection("usuarios");
+        } catch (ex) {
+            console.log(ex);
+            process.exit(1);
+            
+        }
+    }
+
+    async newUser(email,password){
+        try {
+            let user={
+                email:email,
+                password:await bcrypt.hash(password,10),
+                lastlogin:null,
+                lastpasswordchange:null,
+                oldpasswords:[],
+                roles:["public"]
+            }
+            let result = await this.secColl.insertOne(user);
+            return result;
+        } catch (ex) {
+            console.log(ex);
+            throw(ex);
+            
+        }
+    }
+
+    async getByEmail(email){
+        const filter={"email":email};
+        return await this.secColl.findOne(filter);
+    }
+
+    async comparePassword(rawPassword,dbPassword){
+        return await bcrypt.compare(rawPassword,dbPassword);
+    }
+}
+
+module.exports=Sec;
