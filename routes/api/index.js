4d70ae84fd65a0e3a7746153f74cc8f325e54352
natamador19 <73267858+natamador19@users.noreply.github.com>
2021-11-29 18:03:32 -0600

    first commit 29-11-2021

---
+++ routes/api/index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+
+
+const passport = require('passport');
+const passportJWT = require('passport-jwt');
+const extractJWT = passportJWT.ExtractJwt;
+const strategyJWT = passportJWT.Strategy;
+
+passport.use(
+    new strategyJWT(
+        {
+            jwtFromRequest:extractJWT.fromAuthHeaderAsBearerToken(),
+            secretOrKey:process.env.JWT_SECRET
+        },
+        (payload,next)=>{
+            return next(null, payload);
+        }
+    )
+);
+
+const jwtMiddleware=passport.authenticate('jwt',{session:false});
+router.use(passport.initialize());
+var notesRouter= require('./sec/index');
+var secRouter= require('./sec/index');
+
+router.get('/',(req,res,next)=>{
+    res.status(200).json({"msg":"Api"})
+});
+router.use('/sec', secRouter);
+router.use('/swot', jwtMiddleware ,swotRouter);
+
+module.exports=router;
