29fb9a24b055aae3e56ad88f781b6813c243257b
SkyLineHN <allan.fuentes110@gmail.com>
2021-11-30 17:49:38 -0600

    update

--- routes/api/notes/index.js
+++ routes/api/notes/index.js
@@ -3,6 +3,17 @@ var router= express.Router();
 var NotesDao= require('./notes.dao');
 var Notes= new NotesDao();
 
+
+router.get('/all', async (req, res, next)=>{
+    try{
+      const allNoteEntries = await Notes.getAll(req.user._id);
+      return res.status(200).json(allNoteEntries);
+    }catch(ex){
+      console.log(ex);
+      return res.status(500).json({msg:"Error al procesar petición"});
+    }
+  });
+
 router.post('/newNote', async (req,res,next)=>{
     try {
         const {noteTitle,noteContent}=req.body;
@@ -13,6 +24,19 @@ router.post('/newNote', async (req,res,next)=>{
         console.log(ex);
     }
 });
+
+router.delete('/delete/:id', async (req, res, next)=>{
+    try {
+      const {id} = req.params;
+      const result = await Notes.deleteById(id, req.user._id);
+      console.log(result);
+      return res.status(200).json({"msg":"Eliminado OK"});
+    } catch (ex) {
+      console.log(ex);
+      return res.status(500).json({ msg: "Error al procesar petición" });
+    }
+  }); 
+  
 
 
 module.exports=router;
